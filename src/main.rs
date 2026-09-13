95d294c30e887c8746fb6a74af47ba869009adb2
Matheus <m.matheussommer@gmail.com>
2026-09-13 00:04:10 -0300

    scan dentro do if

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,25 @@ struct Cli{
 
     #[arg(short, long )]
     list: Option<String>,
-
+    
 }
 fn main(){
     let args = Cli::parse();
 
     if let Some(single) = args.single{
-
+        scan_ports(&args.address, vec![single]);
+    }
+    if let Some(range) = args.range{
+        let mut split = range.split('-');
+        let start: u16 = match split.next(){
+            Some(s) => s.parse().unwrap(),
+            None => panic!("Valor do range não é válido"),
+        };
+        let mut list = vec![];
+        (start..=end).into_iter().for_each(| x | list.push(x));
+        scan_ports(addr, list);
     }
 }
-fn scan_ports(addr: &string, list: Vec<u16>){
+fn scan_ports(addr: &String, list: Vec<u16>){
 
 }
